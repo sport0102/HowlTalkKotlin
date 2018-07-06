3f16885d1ca68888aacde1bd097295dee6c0dc6d
smlee <sunminlee89@gmail.com>
2018-07-07 00:40:38 +0900

    메세지 나타내기 최종

--- app/src/main/java/com/example/sport0102/myapplication/model/ChatModel.kt
+++ app/src/main/java/com/example/sport0102/myapplication/model/ChatModel.kt
@@ -1,6 +1,6 @@
 package com.example.sport0102.myapplication.model
 
-data class ChatModel(var users:HashMap<String, Boolean>?=HashMap(),var comments:HashMap<String, Boolean>?=HashMap()) {
+data class ChatModel(var users:HashMap<String, Boolean>?=HashMap(),var comments:HashMap<String, Comment>?=HashMap()) {
     companion object {
         data class Comment(var uid: String?=null, var message:String?=null)
     }
