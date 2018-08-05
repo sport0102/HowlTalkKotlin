99ad9e28b68e54c67bf9c05a27d41cf6dab8f643
sport0102 <sunminlee89@gmail.com>
2018-08-05 09:44:05 +0900

    31 그룹메시지 모내기 6

--- app/src/main/java/com/example/sport0102/myapplication/chat/GroupMessageActivity.kt
+++ app/src/main/java/com/example/sport0102/myapplication/chat/GroupMessageActivity.kt
@@ -6,9 +6,12 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.text.TextUtils
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.sport0102.myapplication.R
 import com.example.sport0102.myapplication.model.ChatModel
 import com.example.sport0102.myapplication.model.UserModel
@@ -16,7 +19,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_group_message.*
 import kotlinx.android.synthetic.main.activity_message.*
-import java.util.ArrayList
+import kotlinx.android.synthetic.main.item_message.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class GroupMessageActivity : AppCompatActivity() {
     var users = HashMap<String, UserModel>()
@@ -27,6 +32,8 @@ class GroupMessageActivity : AppCompatActivity() {
     lateinit var dataReference: DatabaseReference
     var valueEventListener: ValueEventListener? = null
     var comments: ArrayList<ChatModel.Companion.Comment> = ArrayList()
+    var simpleDataFormat = SimpleDateFormat("yyyy.MM.dd HH:mm")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,14 +46,17 @@ class GroupMessageActivity : AppCompatActivity() {
             }
 
             override fun onDataChange(p0: DataSnapshot) {
-                users = p0.getValue() as HashMap<String, UserModel>
-                Log.d("users",users.toString())
+                p0.children.forEach {
+                    users.put(it.key!!, it.getValue(UserModel::class.java)!!)
+                }
+                Log.d("users", users.toString())
                 init()
+                group_message_rv.adapter = GroupMessageRecyclerViewAdapter()
+                group_message_rv.layoutManager = LinearLayoutManager(this@GroupMessageActivity)
             }
 
         })
-        group_message_rv.adapter = GroupMessageRecyclerViewAdapter()
-        group_message_rv.layoutManager = LinearLayoutManager(this)
+
     }
 
     fun init() {
@@ -62,8 +72,9 @@ class GroupMessageActivity : AppCompatActivity() {
             }
         }
     }
-    inner class  GroupMessageRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-        init{
+
+    inner class GroupMessageRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        init {
             getMessageList()
         }
 
@@ -85,26 +96,27 @@ class GroupMessageActivity : AppCompatActivity() {
                         readUsersMap.put(key!!, commentModify)
                         comments.add(commentOrigin!!)
                     }
-                    if (!comments.get(comments.size - 1).readUsers.containsKey(uid)) {
-
-                        mFirebaseDatabase.reference.child(resources.getString(R.string.db_chatrooms)).child(destinationRoom!!).child("comments").updateChildren(readUsersMap)
-                                .addOnCompleteListener {
-                                    // 메시지가 갱신
-                                    notifyDataSetChanged()
-                                    group_message_rv.scrollToPosition(comments.size - 1)
-                                }
-
-                    } else {
-                        notifyDataSetChanged()
-                        group_message_rv.scrollToPosition(comments.size - 1)
-                    }
+                    notifyDataSetChanged()
+                    group_message_rv.scrollToPosition(comments.size - 1)
+//                    if (!comments.get(comments.size - 1).readUsers.containsKey(uid)) {
+//                        mFirebaseDatabase.reference.child(resources.getString(R.string.db_chatrooms)).child(destinationRoom!!).child("comments").updateChildren(readUsersMap)
+//                                .addOnCompleteListener {
+//                                    // 메시지가 갱신
+//                                    notifyDataSetChanged()
+//                                    group_message_rv.scrollToPosition(comments.size - 1)
+//                                }
+//
+//                    } else {
+//                        notifyDataSetChanged()
+//                        group_message_rv.scrollToPosition(comments.size - 1)
+//                    }
                 }
 
             })
         }
 
         override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
-            var view = LayoutInflater.from(p0!!.context).inflate(R.layout.item_message,p0,false)
+            var view = LayoutInflater.from(p0!!.context).inflate(R.layout.item_message, p0, false)
             return GroupMessageViewHolder(view)
         }
 
@@ -113,9 +125,32 @@ class GroupMessageActivity : AppCompatActivity() {
         }
 
         override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+            if (comments.size > 0) {
+                if (comments.get(p1).uid == uid) {
+                    p0.itemView.item_message_tv_message.text = comments.get(p1).message
+                    p0.itemView.item_message_tv_message.setBackgroundResource(R.drawable.rightbuble)
+                    p0.itemView.item_message_ll_destination.visibility = View.INVISIBLE
+                    p0.itemView.item_message_ll_main.gravity = Gravity.RIGHT
+//                setReadCounter(p1, p0.itemView.item_message_tv_readcounter_left)
+                } else {
+                    Glide.with(applicationContext).load(users.get(comments.get(p1).uid)!!.profileImageUrl).apply(RequestOptions().circleCrop()).into(p0.itemView.item_message_iv_profile)
+                    p0.itemView.item_message_tv_name.text = users.get(comments.get(p1).uid)!!.userName
+                    p0.itemView.item_message_ll_destination.visibility = View.VISIBLE
+                    p0.itemView.item_message_tv_message.setBackgroundResource(R.drawable.leftbuble)
+                    p0.itemView.item_message_tv_message.text = comments.get(p1).message
+                    p0.itemView.item_message_tv_message.textSize = 25f
+                    p0.itemView.item_message_ll_main.gravity = Gravity.LEFT
+//                setReadCounter(p1, p0.itemView.item_message_tv_readcounter_right)
+                }
+                var unixTime = comments.get(p1).timestamp.toString().toLong()
+                var date = Date(unixTime)
+                simpleDataFormat.timeZone = TimeZone.getTimeZone("Asia/Seoul")
+                var time = simpleDataFormat.format(date)
+                p0.itemView.item_message_tv_timestamp.setText(time)
+            }
         }
 
-        inner class GroupMessageViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+        inner class GroupMessageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         }
 
